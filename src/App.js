7bf0d48dd0b8fe2bb67c1bcb3cbc18d8958b66f5
soni lamkaj <acdsoni@gmail.com>
2020-04-19 22:21:24 +0200

    added card-list component for cards layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
@@ -11,18 +12,18 @@ class App extends Component {
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users").then((response) => {
-      response
-        .json()
-        .then((users) => this.setState({monsters : users}));
+      response.json().then((users) => this.setState({ monsters: users }));
     });
   }
 
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        <CardList>
+          {this.state.monsters.map((monster) => (
+            <h1 key={monster.id}>{monster.name}</h1>
+          ))}
+        </CardList>
       </div>
     );
   }
